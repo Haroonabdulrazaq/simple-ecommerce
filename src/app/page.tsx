1d6374e6aee83a6d28fab166908dd0237cf8b09d
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2024-01-13 16:57:34 +0100

    Add MUI onfiguration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,36 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import { Container, Grid, Hidden, Typography } from "@mui/material";
+import { theme } from "./theme/theme";
+
+const headerContainer = {
+  width: "100%",
+  minHeight: "100vh",
+  border: "solid 1px red",
+};
+
+const topHeader = {
+  background: "primary.main",
+};
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    <Container>
+      <Grid container spacing={2}>
+        <Hidden smDown>
+          <Grid container item xs={12} style={topHeader}>
+            <Grid></Grid>
+            <Grid>
+              <Typography>Follow us and get a chance to win 80% off</Typography>{" "}
+            </Grid>
+            <Grid>
+              {" "}
+              <Typography>Follow us:</Typography>{" "}
+            </Grid>
+          </Grid>
+        </Hidden>
+        <Grid item xs={12}>
+          bottom header
+        </Grid>
+      </Grid>
+    </Container>
   );
 }
---
+++ src/app/theme/colors.ts
@@ -0,0 +1,6 @@
+export const Colors = {
+  primary: "#23A6F0",
+  secondary: "#252B42",
+  white: "#FFFFF",
+  black: "#000000",
+};
